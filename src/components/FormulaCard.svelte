2c8ffdbe3ee2104f1ddb1a84bc2dbd3d6753bc7c
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2025-02-19 21:50:14 +0100

    add more formulas, and scroll to see long formulas

--- src/components/FormulaCard.svelte
+++ src/components/FormulaCard.svelte
@@ -22,10 +22,10 @@
 
 <div
   id={slugify(formula.name)}
-  class="border rounded-lg p-4 shadow hover:shadow-lg transition-shadow relative"
+  class="border rounded-lg p-4 shadow hover:shadow-lg transition-shadow relative overflow-x-auto"
   on:click={toggleDescription}
   style="transition: all 200ms ease;"
-  >
+>
   <a
     href={formula.link}
     target="_blank"
@@ -34,8 +34,9 @@
   >
     {formula.name}
   </a>
+
   <Latex math={formula.formula} />
-  
+
   {#if showDescription}
     <div transition:fade={{ duration: 200 }} class="mt-2 p-2 border rounded bg-gray-100 text-sm">
       {formula.description}
